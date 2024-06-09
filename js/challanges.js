2b7ba0e14e0cc9191b0f8b94566f13a9ebffae97
adanmwassi <adanmwasse@gmail.com>
2024-06-09 15:11:51 +0300

    Add backgrounds array to beginner challenges and add 10 levels

--- js/challanges.js
+++ js/challanges.js
@@ -81,7 +81,89 @@ const challenges = {
           background: "linear-gradient(to right, red, yellow)"
         }
       }
-    ]
+    ],
+    backgrounds: [
+      {
+        level: 1,
+        description: "Set the background color of the box to lightgrey.",
+        solution: "background-color: lightgrey;",
+        styles: {
+          backgroundColor: "lightgrey"
+        }
+      },
+      {
+        level: 2,
+        description: "Set the opacity of the box to 0.5.",
+        solution: "opacity: 0.5;",
+        styles: {
+          opacity: "0.5"
+        }
+      },
+      {
+        level: 3,
+        description: "Set the background color of the box to rgba(255, 0, 0, 0.3).",
+        solution: "background-color: rgba(255, 0, 0, 0.3);",
+        styles: {
+          backgroundColor: "rgba(255, 0, 0, 0.3)"
+        }
+      },
+      {
+        level: 4,
+        description: "Set the background color of the box to lightblue.",
+        solution: "background-color: lightblue;",
+        styles: {
+          backgroundColor: "lightblue"
+        }
+      },
+      {
+        level: 5,
+        description: "Set a background image for the box using the URL 'https://www.w3schools.com/css/img_tree.png'.",
+        solution: "background-image: url('https://www.w3schools.com/css/img_tree.png');",
+        styles: {
+          backgroundImage: "url('https://www.w3schools.com/css/img_tree.png')"
+        }
+      },
+      {
+        level: 6,
+        description: "Set the background image of the box to not repeat.",
+        solution: "background-repeat: no-repeat;",
+        styles: {
+          backgroundRepeat: "no-repeat"
+        }
+      },
+      {
+        level: 7,
+        description: "Set the position of the background image to be centered.",
+        solution: "background-position: center;",
+        styles: {
+          backgroundPosition: "center"
+        }
+      },
+      {
+        level: 8,
+        description: "Set the background color of the element to cyan.",
+        solution: "background-color: cyan;",
+        styles: {
+          backgroundColor: "cyan"
+        }
+      },
+      {
+        level: 9,
+        description: "Set the background image of the box to be fixed in place.",
+        solution: "background-attachment: fixed;",
+        styles: {
+          backgroundAttachment: "fixed"
+        }
+      },
+      {
+        level: 10,
+        description: "Set a linear gradient background from blue to green.",
+        solution: "background-image: linear-gradient(to right, blue, green);",
+        styles: {
+          backgroundImage: "linear-gradient(to right, blue, green)"
+        }
+      }
+    ],
   },
   intermediate : {},
   advanced : {}
